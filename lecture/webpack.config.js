c9472376b40e92b6e5ad8f0e887f697f217fc754
hyjeong <zklfg123@gmail.com>
2022-12-28 03:13:22 +0900

    Chore: 2.끝말잇기 - lecture : webpack 설정 적용

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'wordrelay-setting',
@@ -17,12 +18,24 @@ module.exports = {
             loader: 'babel-loader',
             options: {
                 presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             },
         }],
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
     output : {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js',
+        publicPath: '/dist/',
     }, //출력
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    },
 }
