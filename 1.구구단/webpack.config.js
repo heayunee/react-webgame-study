f7accbb28aec1b34f6e891d521cfbe1654cc7287
hyjeong <zklfg123@gmail.com>
2022-12-28 01:11:18 +0900

    Chore: 2.끝말잇기 - Gugudan : pacakge.json, webpack 설정 적용

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -3,34 +3,25 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map', // hidden-source-map
+  devtool: 'eval', // hidden-source-map
   resolve: {
     extensions: ['.jsx', '.js'],
   },
-
   entry: {
     app: './client',
   },
   module: {
-    rules: [{
+    rules:[{
       test: /\.jsx?$/,
       loader: 'babel-loader',
       options: {
-        presets: [
-          ['@babel/preset-env', {
-            targets: {
-              browsers: ['> 1% in KR'], // browserslist
-            },
-            debug: true,
-          }],
-          '@babel/preset-react',
-        ],
-        plugins: [],
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
       },
     }],
   },
   output: {
-    filename: 'app.js',
     path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
   },
 };
