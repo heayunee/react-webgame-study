27f499f5edf6e365b6d881183be017d9de5db500
hyjeong <zklfg123@gmail.com>
2023-01-04 00:22:58 +0900

    Feat: 3.숫자야구 - es6 (import) 문법으로 적용

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,15 +1,3 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-const { hot } = require('react-hot-loader/root');
-
-const NumberBaseball = require('./NumberBaseball');
-
-const Hot = hot(NumberBaseball);
-
-ReactDom.render(<Hot />, document.querySelector('#root'));
-
-/*
-* ES6 문법 : import 사용
 import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
@@ -19,4 +7,3 @@ import NumberBaseball from './NumberBaseball';
 const Hot = hot(NumberBaseball);
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
-* */
