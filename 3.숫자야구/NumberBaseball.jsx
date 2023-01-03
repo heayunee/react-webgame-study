e66fd5207f4b0c905a7086d0bf5a81a0e32c7ba8
hyjeong <zklfg123@gmail.com>
2023-01-04 03:37:11 +0900

    Feat: 3.숫자야구 - Hooks로 전환하기

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import Try from './Try';
 
 /**
@@ -14,79 +14,65 @@ function getNumbers() {
     }
     return array;
 }
+const NumberBaseball = () => {
+    const [result, setResult] = useState('');
+    const [value, setValue] = useState('');
+    const [answer, setAnswer] = useState(getNumbers());
+    const [tries, setTries] = useState([]);
 
-class NumberBaseball extends Component {
-    state = {
-        result: '',
-        value: '',
-        answer: getNumbers(),   //[1,3,5,7]
-        tries: [],  //react에서 배열 값 넣을 때 push 쓰면 안됨.
-    };
-    onSubmitForm = (e) => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if (this.state.value === this.state.answer.join(``)) {
-            this.setState({
-                result : '홈런!',
-                tries: [...this.state.tries, {try: this.state.value, result: '홈런'}],
+        if (value === answer.join(``)) {
+            setResult('홈런!');
+            setTries((prevTries) => {
+                return [...prevTries, {try: value, result: '홈런'}]
             });
-            alert('게임을 다시 시작합니다.');
-            this.setState({
-                value: '',
-                answer: getNumbers(),
-                tries: [],
-            });
+            alert('게임을 다시 시작합니다!');
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
         } else {
-            const answerArray = this.state.value.split(``).map((v) => parseInt(v));
+            const answerArray = value.split(``).map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if (this.state.tries.length >= 9) { //10번 이상 틀렸을 때
-                this.setState({
-                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다! :)`
-                });
+            if (tries.length >= 9) { //10번 이상 틀렸을 때
+                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다! :)`);
                 alert('게임을 다시 시작합니다.');
-                this.setState({
-                    value: '',
-                    answer: getNumbers(),
-                    tries: [],
-                });
+                setValue('');
+                setAnswer(getNumbers());
+                setTries([]);
             } else {
                 for (let i = 0; i < 4; i += 1) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answerArray[i] === answer[i]) {
                         strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball += 1;
                     }
                 }
-                this.setState({
-                    tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
-                    value :'',
-                })
+                setTries((prevTries) => [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}]);
+                setValue('');
             }
         }
     }
-    onChangeInput = (e) => {
-        this.setState({
-            value: e.target.value,
-        })
+    const onChangeInput = (e) => {
+        setValue( e.target.value);
     }
-    render() {
-        return (
-            <>
-                <h1>{this.state.result}</h1>
-                <form onSubmit={this.onSubmitForm}>
-                    {/* JSX 주석 */}
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-                </form>
-                <div>시도: {this.state.tries.length}</div>
-                <ul>
-                    {this.state.tries.map((v,i) => {
-                        return (
-                            <Try key={`${i + 1} 차 시도 :`} tryInfo={v} index={i} />
-                        )
-                    })}
-                </ul>
-            </>
-        );
-    }
+    return (
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={onSubmitForm}>
+                {/* JSX 주석 */}
+                <input maxLength={4} value={value} onChange={onChangeInput} />
+            </form>
+            <div>시도: {tries.length}</div>
+            <ul>
+                {tries.map((v,i) => {
+                    return (
+                        <Try key={`${i + 1} 차 시도 :`} tryInfo={v} index={i} />
+                    )
+                })}
+            </ul>
+        </>
+    );
 }
 export default NumberBaseball;  //import NumberBaseball;
--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
-class Try extends Component {
-    render() {
-        return (
-            <li>
-                <div>{this.props.tryInfo.try}</div>
-                <div>{this.props.tryInfo.result}</div>
-            </li>
-        )
-    }
+const Try = ({ tryInfo }) => {
+    return (
+        <li>
+            <div>{tryInfo.try}</div>
+            <div>{tryInfo.result}</div>
+        </li>
+    )
 }
 
 export default Try;
