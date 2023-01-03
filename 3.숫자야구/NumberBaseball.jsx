714485d3e77e0158a9caa85797593671e81a77ac
hyjeong <zklfg123@gmail.com>
2023-01-04 01:13:36 +0900

    Feat: 3.숫자야구 - 반복문 적용

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';
+import React, { Component } from 'react';
 
-import NumberBaseball from './NumberBaseball';
+/**
+ *  getNumbers () : function
+ *  숫자 4개를 겹치지 않고, 랜덤하게 뽑아내는 함수
+ */
+function getNumbers() {
 
-const Hot = hot(NumberBaseball);
+}
 
-ReactDom.render(<Hot />, document.querySelector('#root'));
+class NumberBaseball extends Component {
+    state = {
+        result: '',
+        value: '',
+        answer: getNumbers(),
+    };
+    onSubmit = () => {
+
+    }
+    onChangeInput = () => {
+
+    }
+    render() {
+        return (
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
+                </form>
+                <div>시도: {this.state.tries.length}</div>
+                <ul>
+                    {[
+                        { fruit: '사과', taste: '맛있다.' },
+                        { fruit: '딸기', taste: '맛있다.' },
+                        { fruit: '파인애플', taste: '달다.' },
+                        { fruit: '레몬', taste: '시다.' },
+                        { fruit: '배', taste: '시원하다.' },
+                        { fruit: '감', taste: '떫다.' },
+                        { fruit: '사과', taste: '맛없다.' }
+                    ].map((v, i) => {
+                        return (
+                            <li key={v.fruit + v.taste}>{v.fruit} : {i}</li>
+                        )
+                    })}
+                </ul>
+            </>
+        );
+    }
+}
+
+export default NumberBaseball;
