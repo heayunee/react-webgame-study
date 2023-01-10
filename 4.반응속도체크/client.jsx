0734d97eb213e990e4a1fd44b2e202482e627104
hyjeong <zklfg123@gmail.com>
2023-01-11 01:32:51 +0900

    Feat: 4.반응속도체크 - React 조건문 (삼항연산자 or 함수형), 반응속도 체크 기능 적용

---
+++ 4.반응속도체크/client.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import * as ReactDom from 'react-dom/client';
+
+import ResponseCheck from'./ResponseCheck';
+ReactDom.createRoot(document.querySelector('#root')).render(<ResponseCheck/>);
